031c3b52c486f582859d808afc3090b845c98510
Adans Klevanskis <adans_klevanskis@hotmail.com>
2016-05-30 11:21:47 -0300

    Update docpad.js

--- docpad.js
+++ docpad.js
@@ -24,7 +24,7 @@ module.exports = {
     callToAction: {
         text: "Inscreva-se gratuitamente!",
         icon: "arrow-right",
-        link: "https://docs.google.com/a/ciandt.com/forms/d/1uyaPTsdRki7Sh-vgCoa7yafeaqJcHaePggAr8nLdgQE/edit"
+        link: "http://goo.gl/forms/nnGu8qQlsBEeUtA23"
     },
 
     // Site info
